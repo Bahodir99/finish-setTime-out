ec142b4631ebf478870b5d0e97182978af99d8a3
Bahodir_99 <bahodirataboyev7@gmail.com>
2023-02-08 00:10:55 +0500

    finished

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 let elForm = document.querySelector('.form')
 let elInp = document.querySelector('.inp')
-
+let elTitle = document.querySelectorAll('title')
+let elMinutes = document.querySelector('.minutes')
+let elSeconds = document.querySelector('.seconds')
+let elDiv = document.querySelector('.date')
 
 // let timeOut = setTimeout((e)=>{
 //     console.log('ok');
@@ -22,26 +25,38 @@ let elInp = document.querySelector('.inp')
 // })
 
 
-let a = 10
+function times(){
+    let nov = new Date()
+    let elHourse = document.createElement('h3')
+    elHourse.innerHTML = nov.getHours()
+    elDiv.appendChild(elHourse)
+    console.log(elHourse);
+}
 
-const setTime = setInterval((e)=>{
-    console.log(a);
-    a -= 1
-    if (a < 0 ){
-        clearInterval(setTime)
-        
-        b = 10
-        const setTime2 = setInterval(()=>{
-        console.log(b);
-        b -= 1
-        if (b < 0){
-        clearInterval(setTime2)
-    }
 
-},1000)
-    }
-
-},1000)
-
+elForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    let a = elInp.value * 60
+    
+    const setTime = setInterval((e)=>{
+        console.log(a)    
+        a -= 1
+        if (a < 0 ){
+            clearInterval(setTime)
+            
+            b = 59
+            const setTime2 = setInterval(()=>{
+            console.log(b);
+            b -= 1
+            if (b < 0){
+            clearInterval(setTime2)
+            alert('Vaqt tugadi')
+        }
+    
+    },1000)
+        }
+    
+    },1000)
+})
 
 
